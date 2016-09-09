8cb33adf27992a871ac4c79faa0c600c0acd9cf8
刘金烨 <75710541@qq.com>
2016-09-09 15:00:45 +0800

    Update entrypoint.sh

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -7,18 +7,18 @@ cp -f dataman.conf.temp dataman.conf
 LOGSTASH_PORT=${LOGSTASH_PORT:-5011}
 ES_HOST=${ES_HOST:-127.0.0.1}
 ES_PORT=${ES_PORT:-9200}
-ES_STDOUT_LEVEL=${ES_STDOUT_LEVEL:-""} # stdout { codec => rubydebug }
-ES_WORKERS=${ES_WORKERS:-8}
-ES_FLUSH_SIZE=${ES_FLUSH_SIZE:20000}
-ES_IDLE_FLUSH_TIME=${ES_IDLE_FLUSH_TIME:-10}
+LS_STDOUT_LEVEL=${LS_STDOUT_LEVEL:-""} # stdout { codec => rubydebug }
+LS_WORKERS=${LS_WORKERS:-8}
+LS_FLUSH_SIZE=${LS_FLUSH_SIZE:20000}
+LS_IDLE_FLUSH_TIME=${LS_IDLE_FLUSH_TIME:-10}
 
 sed -i 's/--LOGSTASH_PORT--/'$LOGSTASH_PORT'/g' dataman.conf
 sed -i 's/--ES_HOST--/'$ES_HOST'/g' dataman.conf
 sed -i 's/--ES_PORT--/'$ES_PORT'/g' dataman.conf
-sed -i 's/--ES_STDOUT_LEVEL--/'$ES_STDOUT_LEVEL'/g' dataman.conf
-sed -i 's/--ES_WORKERS--/'$ES_WORKERS'/g' dataman.conf
-sed -i 's/--ES_FLUSH_SIZE--/'$ES_FLUSH_SIZE'/g' dataman.conf
-sed -i 's/--ES_IDLE_FLUSH_TIME--/'$ES_IDLE_FLUSH_TIME'/g' dataman.conf
+sed -i 's/--LS_STDOUT_LEVEL--/'$LS_STDOUT_LEVEL'/g' dataman.conf
+sed -i 's/--LS_WORKERS--/'$LS_WORKERS'/g' dataman.conf
+sed -i 's/--LS_FLUSH_SIZE--/'$LS_FLUSH_SIZE'/g' dataman.conf
+sed -i 's/--LS_IDLE_FLUSH_TIME--/'$LS_IDLE_FLUSH_TIME'/g' dataman.conf
 
 if [ "x$STDOUTNULL" == "xtrue" ];then
 	echo "stdout output /dev/null."
